401e387427834af7a670c3ccf2dc62715897dc06
Guilherme Vilatoro Taglianeti <79532050+Gui153@users.noreply.github.com>
2023-09-14 00:08:18 -0400

    Finished_Chapter4
    Went through the tutorial chapter 4 and did not do deployment

--- web/src/layouts/BlogLayout/BlogLayout.jsx
+++ web/src/layouts/BlogLayout/BlogLayout.jsx
@@ -1,14 +1,32 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import { useAuth } from 'src/auth'
+
 const BlogLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
   return (
     <>
       <header>
         <h1>
           <Link to={routes.home()}>Gui Blog</Link>
         </h1>
+        {isAuthenticated ? (
+          <div>
+            <span>logged in as {currentUser.email}</span>{' '}
+            <button type="button" onClick={logOut}>
+              LogOut
+            </button>
+          </div>
+        ) : (
+          <Link to={routes.login()}>login</Link>
+        )}
+
         <nav>
           <ul>
+            <li>
+              <Link to={routes.home()}>About</Link>
+            </li>
             <li>
               <Link to={routes.about()}>About</Link>
             </li>
